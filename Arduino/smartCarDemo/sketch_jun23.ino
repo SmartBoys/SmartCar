dfe759dd602441c8777dc5e7f92f53222e7aa488
shengge <502204827@qq.com>
2014-07-19 13:02:13 +0800

    Update sketch_jun23.ino
    update some commod

--- Arduino/smartCarDemo/sketch_jun23.ino
+++ Arduino/smartCarDemo/sketch_jun23.ino
@@ -2,6 +2,20 @@
 
 // Blueduino comunicate with BLE module through pin11, pin12 as soft serial.
 SoftwareSerial mySerial(11, 12); //RX,TX
+/**
+         FORWARD = "f";
+	 BACK = "b";
+	 FORWARD_LEFT = "l";
+	 FORWARD_RIGHT = "r";
+	 ORIGIN_LEFT = "z";
+	 ORIGIN_RIGHT = "y";
+	 BACK_LEFT = "n";
+	 BACK_RIGHT = "m";
+	 STOP = "s";
+	 LIGHT_ON = "o";
+	 LIGHT_OFF = "c";
+	 FLASH_LIGHT_ON = "t";
+*/
 
 char tmp; 
 int ledPin = 6;
@@ -40,16 +54,36 @@ void loop() {
       analogWrite(leftPin,0);
       analogWrite(ledPin, 255);
       analogWrite(rightPin,255);
-   }  else if(tmp == 'r') {
-       analogWrite(backPin,100);
+   }  else if(tmp == 'y') {
+       analogWrite(backPin,0);
       analogWrite(leftPin,255);
        analogWrite(ledPin, 0);
       analogWrite(rightPin,0);
-   }  else if(tmp == 'l') {
+   }  else if(tmp == 'z') {
        analogWrite(backPin,255);
-      analogWrite(leftPin,100);
+      analogWrite(leftPin,0);
       analogWrite(ledPin, 0);
       analogWrite(rightPin,0);
+   }  else if(tmp == 'l') {
+       analogWrite(backPin,255);
+      analogWrite(leftPin,80);
+      analogWrite(ledPin, 0);
+      analogWrite(rightPin,0);
+   }  else if(tmp == 'r') {
+       analogWrite(backPin,80);
+      analogWrite(leftPin,255);
+      analogWrite(ledPin, 0);
+      analogWrite(rightPin,0);
+   }  else if(tmp == 'n') {
+       analogWrite(backPin,0);
+      analogWrite(leftPin,0);
+      analogWrite(ledPin, 255);
+      analogWrite(rightPin,80);
+   }  else if(tmp == 'm') {
+       analogWrite(backPin,0);
+      analogWrite(leftPin,0);
+      analogWrite(ledPin, 80);
+      analogWrite(rightPin,255);
    }
     
    // Serial.println(tmp);
