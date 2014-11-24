e33353a4e71fdd522d60dbe72bf4d605c23becc7
liyusheng <502204827@qq.com>
2014-11-24 16:21:41 +0800

    test

---
+++ android/test/ios/PhoneCar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhoneCar
+//
+//  Created by 潘威 on 14-6-29.
+//  Copyright (c) 2014年 中晋联合技术(北京)有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ android/test/ios/PhoneCar/ViewController.m
@@ -0,0 +1,50 @@
+//
+//  ViewController.m
+//  MyBluetooth
+//
+//  Created by s on 14-2-11.
+//  Copyright (c) 2014年 sunward. All rights reserved.
+//
+
+#import "CoreBluetooth/CoreBluetooth.h"
+#import "ViewController.h"
+
+@interface ViewController ()
+
+
+@end
+
+@implementation ViewController 
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+#pragma mark - 隐藏状态拦
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+@end
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
